fe04cb238620d973c840e63d78b132953f2b21ef
unknown <aadil.shabir13@gmail.com>
2021-12-11 13:17:22 +0500

    Style Home and Skills Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import logo from '../AB-Memon.jpeg';
+import {makeStyles} from '@material-ui/core/styles';
+import {Typography} from '@mui/material';
+
+import styles from '../styles/Home.module.css';
+
+const useStyles = makeStyles({
+  container: {
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+  },
+  logoHolder: {
+    margin: '1.5rem',
+  },
+  logo: {
+    borderRadius: '180px',
+    width: '250px',
+    height: '250px',
+  },
+  typography: {
+    position: 'relative',
+    color: '#11001c',
+    webkitTextStroke: '0.3vw #329d52',
+  },
+  descContainer: {
+    margin: '1.5rem',
+  },
+});
 
 const Home: React.FC = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h2>This is Home Page</h2>
+    <div className={classes.container}>
+      <div className={classes.logoHolder}>
+        <img src={logo} alt="logo" className={classes.logo} />
+      </div>
+      <Typography
+        color="silver"
+        variant="h4"
+        style={{fontFamily: 'Readex Pro'}}
+      >
+        AB Memon
+      </Typography>
+      <div className={styles.textContainer}>
+        <Typography
+          data-text="Web Developer..."
+          className={styles.h2}
+          variant="h3"
+        >
+          Web Developer...
+        </Typography>
+      </div>
+      <Typography variant="h4" color="khaki" style={{fontFamily: 'Wallpoet'}}>
+        About
+      </Typography>
+      <div className={classes.descContainer}>
+        <Typography variant="h5" color="whitesmoke">
+          I am an ambitious Software Engineering Student and
+          <br /> a driven individual with the ability to adapt different
+          <br />
+          situations and I have potential to grow self.
+        </Typography>
+      </div>
     </div>
   );
 };
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import {Typography} from '@mui/material';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  container: {
+    margin: '2rem',
+    textAlign: 'center',
+  },
+});
 
 const Work: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <h3>This is Work Page</h3>
+      <div className={classes.container}>
+        <Typography
+          variant="h4"
+          color="tomato"
+          style={{fontFamily: 'Readex Pro'}}
+        >
+          Recent Work
+        </Typography>
+      </div>
+      <div>
+        <Typography
+          variant="h4"
+          color="tomato"
+          style={{fontFamily: 'Readex Pro'}}
+        >
+          Boosted - Productivity App
+        </Typography>
+        <Typography
+          variant="h6"
+          color="white"
+          style={{fontFamily: 'Readex Pro'}}
+        >
+          A productivity app where users can track records of their time <br />
+          in different projects and get daily, weekly, and monthly reports.{' '}
+          <br /> This application is made with React and Typescript in the
+          frontend <br /> while REST API and Firebase in the backend.
+        </Typography>
+      </div>
     </div>
   );
 };
